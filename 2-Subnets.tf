62ee2aff1b2d83cc9b0a2607542a994f3ed5863e
Clou33 <148802515+Clou33@users.noreply.github.com>
2023-11-13 19:45:27 -0500

    Add files via upload

---
+++ 2-Subnets.tf
@@ -0,0 +1,85 @@
+#These are   for  public
+
+resource "aws_subnet" "public-ap-east-1a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.77.1.0/24"
+  availability_zone       = "ap-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-east-1a"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
+
+resource "aws_subnet" "public-ap-east-1b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.77.2.0/24"
+  availability_zone       = "ap-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-east-1b"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
+
+
+resource "aws_subnet" "public-ap-east-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.77.3.0/24"
+  availability_zone       = "ap-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-ap-east-1c"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-ap-east-1a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.77.11.0/24"
+  availability_zone = "ap-east-1a"
+
+  tags = {
+    Name    = "private-ap-east-1a"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
+
+resource "aws_subnet" "private-ap-east-1b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.77.12.0/24"
+  availability_zone = "ap-east-1b"
+
+  tags = {
+    Name    = "private-ap-east-1b"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
+
+
+resource "aws_subnet" "private-ap-east-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.77.13.0/24"
+  availability_zone = "ap-east-1c"
+
+  tags = {
+    Name    = "private-ap-east-1c"
+    Service = "application1"
+    Owner   = "Straw Hats!"
+    Island  = "Laugh Tale"
+  }
+}
